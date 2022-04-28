96c6c2f1518f267cb2dc85f25301b48646c55ee3
Tahmid Hasan <wdtahmid@gmail.com>
2022-04-29 02:04:06 +0600

    Configuer routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AboutUs from './components/AboutUs/AboutUs';
+import Blogs from './components/Blogs/Blogs';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import Footer from './components/Shared/Footer/Footer';
 import Header from './components/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='aboutus' element={<AboutUs></AboutUs>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
