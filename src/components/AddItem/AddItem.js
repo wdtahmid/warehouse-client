689a679309f30232c57676b5ba24dd26164828a6
Tahmid Hasan <wdtahmid@gmail.com>
2022-04-29 15:54:08 +0600

    Add an item successfully and showing sucess message

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 
 const AddItem = () => {
+    const [success, setSuccess] = useState('')
+
+    const handleInsetInventory = event => {
+        event.preventDefault();
+
+        const productName = event.target.productName.value;
+        const productPrice = event.target.productPrice.value;
+
+        const inventories = {
+            productName,
+            productPrice
+        }
+
+        const url = 'http://localhost:5000/additem';
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+
+            body: JSON.stringify(inventories)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    setSuccess('the item has benn added successfully!')
+                }
+            })
+        console.log(inventories);
+    }
     return (
-        <div>
+        <div className='w-25 mx-auto mt-5'>
             <h2>Add item</h2>
 
-            <Form>
+            <Form onSubmit={handleInsetInventory}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
 
                     <Form.Control type="productName" name='productName' placeholder="Product Name" />
@@ -19,6 +49,8 @@ const AddItem = () => {
                     Insert This Product
                 </Button>
             </Form>
+
+            <p>{success}</p>
         </div>
     );
 };
