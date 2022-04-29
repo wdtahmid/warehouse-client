9435c8c08f9d21bd9560bb926719917bbb543bd9
Tahmid Hasan <wdtahmid@gmail.com>
2022-04-29 12:15:48 +0600

    add manage inventories page

---
+++ src/components/ManageInventories/ManageInventories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageInventories = () => {
+    return (
+        <div>
+            <h2>Manage Inventories</h2>
+        </div>
+    );
+};
+
+export default ManageInventories;
